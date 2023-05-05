a7b13bba580bf3a1c63823c34f3061a68694843a
dadsec-dev <geecypher41@gmail.com>
2023-05-05 01:58:00 +0100

    updating search functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,7 +4,16 @@ import {copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+        <div className='flex flex-col w-full gap-2'>
+            <form className='relative flex justify-center items-center' onSubmit={() => {}}>
+
+              <img src={linkIcon} alt='link_icon' className='absolute left-0 my-2 ml-3 w-5'/>
+
+              <input type='url' placeholder='Enter url-link to an article' value='' className='url_input peer' onChange={() => {}} required/>
+            </form>
+        </div>
+    </section>
   )
 }
 
